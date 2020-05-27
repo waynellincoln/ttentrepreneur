a789e762767e43fa21d1201957d0439ced1604eb
waynellincoln <57865047+waynellincoln@users.noreply.github.com>
2020-05-26 20:33:21 -0400

    Pagination
    Pagination

--- index_old.php
+++ index_old.php
@@ -20,69 +20,12 @@
                     <small></small>
                 </h1>
                 
-                <?php
-                //pagination code starts here; pagination code starts here; pagination code starts here
-                //Pagination - 3rd thing - collect get request and process it
-                
-                $per_page   = 10;           //code to indicate how much posts you want on the page
-                
-                if(isset($_GET['page'])) {
-                    
-                    
-                    
-                    $page = $_GET['page'];
-                    
-                }else {
-                    
-                    $page = "";
-                }
-                
-                if($page == "" || $page == 1) {
-                    
-                    $page_1 = 0;
-                    
-                }else {
-                    
-                    $page_1 = ($page * $per_page) - $per_page;
-                }
-                
-                
-                
-                 
-                //Pagination - First thing - find out how many posts we have 
-                $query = "SELECT * 
-                          FROM posts";    
-                
-                $total_posts = mysqli_query($con, $query);
-                
-                $rows_counted = mysqli_num_rows($total_posts);
-                
-                //To find out how much pages we want if 5 posts to a page
-                $pages_needed = ceil($rows_counted / $per_page);
-                    
-                
-                
-                
-                
-                
-                
-                
-                
-                ?>
-                
-                
-                
-                
-                
-                
-                
                 <!-- To allow posts to get their information from the post table in the database -->
                 <?php 
                 
                     $query = "SELECT *
                               FROM posts
-                              WHERE post_status = 'published' 
-                              LIMIT $page_1, $per_page ";               //5 is the amount on posts on each page
+                              WHERE post_status = 'published' ";
                 
                     $select_all_posts_query = mysqli_query($con, $query);
                 
@@ -99,7 +42,7 @@
                         
                      //Break out PHP to accommodate looping through all the posts in database 
                     ?>
-                           <h2><?php echo $pages_needed; ?></h2> 
+                    
                         <!-- First Blog Post -->
                         <h2>
                             <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
@@ -130,35 +73,11 @@
         <!-- Blog Sidebar Widgets Column -->
   <?php include "includes/sidebar.php" ; ?>            
             
+
         </div>
         <!-- /.row -->
+
         <hr>
-         
-        <!-- code to start dynamic pagination links using ul and a loop-->
-        <ul class="pager">
-        <?php
-            
-            //Pagination - 2nd thing - put in the page numbers and let each number send get request to index page
-            for($i = 1; $i <= $pages_needed; $i++) {
-                
-                if($i == $page) {
-                    
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-                    
-                }else {
-                    
-                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-                }
-                
-                
-            }
-            
-           
-        ?>   
-            
-            
-        </ul>
-        
 
         <!-- Footer -->
    <?php include "includes/footer.php" ; ?>     
